4e1c9f3a88163487d0977413bee7cfef03de532c
Rob787 <rob.vermeij@exsyn.com>
2024-04-12 13:25:22 +0200

    ok

--- file_uploading/test.py
+++ file_uploading/test.py
@@ -1,40 +1,68 @@
-#import knime.scripting.io as knio
-
-# This example script creates an output table containing randomly drawn integers using numpy and pandas.
-import requests
+import knime.scripting.io as knio
 import os
+import requests
+import pandas as pd # Used for data frame
 
-file_path = r'C:\Users\filipe.pedro\Downloads\File_0.png'
+file_path = knio.flow_variables['File-Path']
 file_size = os.path.getsize(file_path)
 
 print("File size:", file_size, "bytes")
 
-url_get = f"https://ops.exsyn.com/projects/api/v1/pendingfiles/presignedurl.json?fileName=File_0.png&fileSize={file_size}"
+
+############PHASE 1 - GET REQUEST############
+url_get = knio.flow_variables['Phase1-URL']
 
 payload = {}
 headers = {
-'Authorization': 'Basic teamwork_token'
+  'Authorization': 'Basic tokenXXXX'
 }
 
 response_get = requests.request("GET", url_get, headers=headers, data=payload)
 
-url_put = response_get.json()['url']
-print(url_put)
-filename = 'File_0.png'
-filepath = file_path
+
+
+############PHASE 2 - PUT REQUEST############
+url_put =  response_get.json()['url']
+filename = knio.flow_variables['Filename']
+filepath = knio.flow_variables['File-Path']
+
+
+session = requests.Session()
 
 payload = {}
 files=[
-('',(filename,open(filepath,'rb'),'image/png'))
+  ('',(filename,open(filepath,'rb'),'image/png'))
 ]
-headers = {
-'X-Amz-Acl': 'public-read',
-'Content-Length': str(file_size)
+
+file_size_payload = os.path.getsize(file_path)
+
+headers_put = {
+  'X-Amz-Acl': 'public-read',
+  'Content-Length': str(file_size),
+  'Host': 'tw-eu-bucket.s3-accelerate.amazonaws.com'
 }
 
-response = requests.request("PUT", url_put, headers=headers, data=payload, files=files)
+request = requests.Request('PUT', url_put, headers=headers_put, data=payload, files=files)
 
-print(response.status_code)
+prepared_request = request.prepare()
 
+prepared_request.headers['Content-Length'] = str(file_size)
+
+
+#response = requests.put(url_put, headers=headers_put, data=payload, files=files)
+response = session.send(prepared_request)
+
+
+data_table = pd.DataFrame({'HTTP Status': [response.status_code], 'PH2-PUT-URL': [url_put]})
+
+knio.output_tables[0] = knio.Table.from_pandas(data_table)
+
+
+print('Put Request', request.headers)
+print('Headers variable', headers)
+print(str(file_size))
+#print(prepared_request.body)
+#print(url_get)
+#print(url_put)
 #print(response_get.text)
-print(response.text)
+print(response.status_code)
